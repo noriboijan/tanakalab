0f087a5f5efbddc3a6d7e4c797506d15cb500ab0
Taku YAMAZAKI <taku.yamazaki.jp@gmail.com>
2015-04-16 18:38:57 +0900

    1st commit.

---
+++ js/import.js
@@ -0,0 +1,6 @@
+document.write('<script src="/js/jquery.min.js"></script>');
+document.write('<script src="/js/template.js"></script>');
+document.write('<script src="/js/bootstrap.min.js"></script>');
+document.write('<link href="/css/bootstrap.css" rel="stylesheet">');
+document.write('<link href="/css/navbar-fixed-top.css" rel="stylesheet">');
+document.write('<link href="/css/footer.css" rel="stylesheet">');
---
+++ js/template.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+  $.ajax({
+    type: 'GET',
+    url: "/header.html",
+    dataType: 'html',
+    success: function(data){
+      $('body').prepend(data);
+      var loc = location.href;
+      var file = loc.substring(loc.lastIndexOf("/")+1,loc.length);
+      var $dir = location.href.split("/");
+      var $dir2 = $dir[$dir.length -2];
+      if($dir2 == 'jpn'){
+        $("#navbar li.jpn").addClass("active");
+      }else{
+        $("#navbar li.eng").addClass("active");
+      }
+      if(file ==  'index.html'){
+        $("#navbar li.home").addClass("active");
+      }
+      else if(file == 'research.html'){
+        $("#navbar li.research").addClass("active");
+      }
+      else if(file == 'access.html'){
+        $("#navbar li.access").addClass("active");
+      }
+      else if(file == 'member.html'){
+        $("#navbar li.member").addClass("active");
+      }
+      else if(file == 'entrance.html'){
+        $("#navbar li.entrance").addClass("active");
+      }
+    }
+  });
+  $.ajax({
+    type: 'GET',
+    url: "/footer.html",
+    dataType: 'html',
+    success: function(data){
+      $('body').prepend(data);
+    }
+  });
+});
