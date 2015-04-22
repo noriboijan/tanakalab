9e4600dbd8823c89cb710a025ba11256a091f1c0
Taku YAMAZAKI <taku.yamazaki.jp@gmail.com>
2015-04-22 20:07:59 +0900

    page-header等修正

--- js/member - コピー (5).js
+++ js/member - コピー (5).js
@@ -5,7 +5,7 @@ $(function(){
     var test = true;
     $(csv).each(function(){
       if(this[0] == "-" && test == true){
-        list += '<div class="panel panel-success">';
+        list += '<div class="panel panel-default">';
         list += '<div class="panel-heading">';
         list +=  '<h3 class="panel-title">' +this[1]+ '</h3>';
         list += '</div>';
@@ -14,30 +14,30 @@ $(function(){
       }
       else if(this[0] == "-"){
         list += '</div></div>';
-        list += '<div class="panel panel-success">';
+        list += '<div class="panel panel-default">';
         list += '<div class="panel-heading">';
         list +=  '<h3 class="panel-title">' +this[1]+ '</h3>';
         list += '</div>';
         list += '<div class="panel-body">';
       }
       else if(this[0] == "+"){
-        list += '</div><ul class="list-group"><li class="list-group-item well well-sm">'+this[1]+'</li></ul><div class="panel-body">';
+        list += '</div><ul class="list-group"><li class="list-group-item">'+this[1]+'</li><div class="panel-body">';
       }
       else{
         list += '<div class="col-xs-12">';
-        list += '<div class="col-xs-11">'+this[0];
+        list += '<div class="col-xs-5 col-ms-5">'+this[0]+'</div>';
         if(this[4]){
-          list += '（'+this[4]+'）</div>';
+          list += '<div class="col-xs-6 col-ms-6">'+this[4]+'</div>';
         }
         else{
-          list += '</div>';
+          list += '<div class="col-xs-6 col-ms-6">　</div>';
         }
-        list += '<div class="col-xs-1 text-center">';
         if(this[3]){
+          list += '<div class="col-xs-1 col-ms-1">';
           list += '<a href="'+this[3]+'"><span class="glyphicon glyphicon-home" aria-hidden="true"></span></a></div>';
         }
         else{
-          list += '</div>';
+          list += '<div class="col-ms-1">　</div>';
         }
         list += '</div>';
       }
