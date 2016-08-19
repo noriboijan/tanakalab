2df152196e49fd45ba608bcc7e80325005938ff0
Taku Yamazaki <taku.yamazaki.jp@gmail.com>
2016-08-19 16:53:13 +0900

    Fix a syntax error

--- js/news_from_csv.js
+++ js/news_from_csv.js
@@ -7,7 +7,7 @@ $(function(){
     success: function(data){
       csv_array = $.csv()(data);
       var article = '<table><tbody>';
-      for (var i = 1; i < csv_array.length; i++) {
+      for (var i = 1; i < csv_array.length; i++){
         article += "<tr><td style=\"width: 140px\;vertical-align: top\;\">";
         article += csv_array[i][0]
         article += '</td><td>';
@@ -16,6 +16,6 @@ $(function(){
       }
       article += '</tbody></table>';
       $(target).append(article);
-    };
+    }
   });
 });
