3cc4c99cb024a422765c2454e64e65e64ce35df0
Taku Yamazaki <taku.yamazaki.jp@gmail.com>
2016-08-19 17:02:38 +0900

    Add script for getting csv

--- js/news_from_csv.js
+++ js/news_from_csv.js
@@ -7,7 +7,7 @@ $(function(){
     success: function(data){
       csv_array = $.csv()(data);
       var article = '<table><tbody>';
-      for (var i = 1; i < csv_array.length; i++){
+      for (var i = 0; i < csv_array.length; i++){
         article += "<tr><td style=\"width: 140px\;vertical-align: top\;\">";
         article += csv_array[i][0]
         article += '</td><td>';
